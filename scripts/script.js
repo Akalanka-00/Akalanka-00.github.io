32783e0ca6088d89dbf5fc26906895ad45cc22d7
Shenal Akalanka <shenalakalanka513@gmail.com>
2022-04-07 16:16:03 +0530

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -42,7 +42,7 @@ $(document).ready(function(){
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Student", "Mobile app devoloper"],
+        strings: ["Web devoloper", "Mobile app devoloper"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
