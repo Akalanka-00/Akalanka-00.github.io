4942a25c15010b7b9373f787414d918866122138
Shenal Akalanka <shenalakalanka513@gmail.com>
2022-04-07 16:20:30 +0530

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Student", "Mobile app devoloper"],
+        strings: ["Web devoloper", "Mobile app devoloper"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
